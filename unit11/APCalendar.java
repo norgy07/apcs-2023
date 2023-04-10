a4d8dba9301d982c7867d06e10c9090efc634df8
norgy07 <127888520+norgy07@users.noreply.github.com>
2023-04-10 21:42:00 +0000

    Finished FRQ

--- unit11/APCalendar.java
+++ unit11/APCalendar.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 public class APCalendar {
 
 	/** Returns true if year is a leap year and false otherwise. */
-	private static boolean isLeapYear(int year) { /* implementation not shown */
+	private static boolean isLeapYear(int year) { 
 		return LocalDate.of(year, 1, 1).isLeapYear();
 	}
 
@@ -17,14 +17,21 @@ public class APCalendar {
 	 */
 	public static int numberOfLeapYears(int year1, int year2) {
 		/* to be implemented in part (a) */
-		return -1; // replace me!
+		int count = 0;
+		if(0 <= year1 && year1 <= year2){ 
+			for (int s = year1; s <= year2; s++){
+				if(isLeapYear(s))
+					count++;
+			}
+		}
+		return count;
 	}
 
 	/**
 	 * Returns the value representing the day of the week for the first day of year,
 	 * where 0 denotes Sunday, 1 denotes Monday, ..., and 6 denotes Saturday.
 	 */
-	private static int firstDayOfYear(int year) { /* implementation not shown */
+	private static int firstDayOfYear(int year) { 
 		return LocalDate.of(year, 1, 1).getDayOfWeek().getValue();
 	}
 
@@ -33,7 +40,7 @@ public class APCalendar {
 	 * Returns 1 for January 1 (month = 1, day = 1) of any year.
 	 * Precondition: The date represented by month, day, year is a valid date.
 	 */
-	private static int dayOfYear(int month, int day, int year) { /* implementation not shown */
+	private static int dayOfYear(int month, int day, int year) {
 		return LocalDate.of(year, month, day).getDayOfYear();
 	}
 
@@ -45,7 +52,7 @@ public class APCalendar {
 	 */
 	public static int dayOfWeek(int month, int day, int year) {
 		/* to be implemented in part (b) */
-		return -1; // replace me!
+		return (dayOfYear(month, day, year) + firstDayOfYear(year) - 1) % 7;
 	}
 
 	public static void check(boolean test) throws AssertionError {
