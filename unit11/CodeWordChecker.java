c4a73fa00dd41f4e1859fbea1673d0bf0560d333
norgy07 <127888520+norgy07@users.noreply.github.com>
2023-04-13 03:52:01 +0000

    Work have two errors not sure why

--- unit11/CodeWordChecker.java
+++ unit11/CodeWordChecker.java
@@ -9,12 +9,32 @@ interface StringChecker {
 
 public class CodeWordChecker implements StringChecker {
 
-    // TODO implement me!
+    private int min;
+    private int max;
+    private String no;
+
+    public CodeWordChecker(int m, int m2, String n){
+        min = m;
+        max = m2;
+        no = n;
+    }
+
+    public CodeWordChecker(String n){
+        no = n;
+    }
 
     /** Returns true if str is valid. */
     public boolean isValid(String str) {
-        // TODO
-        return false; // replace me!
+        boolean good = true;
+        if(str.length() >= min && str.length() <= max){
+            for(int index = 0; index < str.length(); index++){
+                if (Character.toString(str.charAt(index)) == no)
+                    good = false;
+                else
+                    good = true;
+            }
+        }
+        return good;
     }
 
     static boolean check(boolean test) throws AssertionError {
@@ -25,21 +45,21 @@ public class CodeWordChecker implements StringChecker {
 
     public static void main(String[] args) {
         // uncomment the following lines to test your code
-        // StringChecker sc1 = new CodeWordChecker(5, 8, "$");
-        // check(sc1.isValid("happy"));
-        // check(!sc1.isValid("happy$"));
-        // check(!sc1.isValid("Code"));
-        // check(!sc1.isValid("happyCode"));
-        // check(!sc1.isValid("happy$Code"));
-        // check(!sc1.isValid("Code$happy"));
+         StringChecker sc1 = new CodeWordChecker(5, 8, "$");
+        check(sc1.isValid("happy"));
+        check(!sc1.isValid("happy$"));
+        check(!sc1.isValid("Code"));
+        check(!sc1.isValid("happyCode"));
+        check(!sc1.isValid("happy$Code"));
+        check(!sc1.isValid("Code$happy"));
 
-        // StringChecker sc2 = new CodeWordChecker("pass");
-        // check(sc2.isValid("MyPass"));
-        // check(!sc2.isValid("Mypassport"));
-        // check(!sc2.isValid("happy"));
-        // check(!sc2.isValid("1,000,000,000,000,000"));
+        StringChecker sc2 = new CodeWordChecker("pass");
+        check(sc2.isValid("MyPass"));
+        check(!sc2.isValid("Mypassport"));
+        check(!sc2.isValid("happy"));
+        check(!sc2.isValid("1,000,000,000,000,000"));
 
-        // System.out.println("Happy Panda! \uD83D\uDC3C");
+        System.out.println("Happy Panda! \uD83D\uDC3C");
     }
 
 }
