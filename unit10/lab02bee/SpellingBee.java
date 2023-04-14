76fb67425b14829286c5429c34c08015f84965e3
norgy07 <127888520+norgy07@users.noreply.github.com>
2023-04-14 22:35:15 +0000

    spellbee

--- unit10/lab02bee/SpellingBee.java
+++ unit10/lab02bee/SpellingBee.java
@@ -1,6 +1,4 @@
 
-
-
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
@@ -19,24 +17,27 @@ public class SpellingBee {
 
     // does it use only the allowed letters?
     // does it use the required letter?
-    // is it at least 4 characters? 
+    // is it at least 4 characters?
     public boolean checkWord(String word) {
+        if (word.length() < 4)
+            return false;
         boolean hasReq = false;
-        boolean fromLets = false;
-        char[] wordsies = new char[word.length()];
-        for (int l = 0; l < word.length(); l++){
-            wordsies[l] = word.charAt(l);
+        for (int l = 0; l < word.length(); l++) {
             if (word.charAt(l) == mustUse)
-              hasReq = true;
-            for (int is = 0; is < letters.length; is++){
-                while(word.charAt(l)== letters[is])
-                    fromLets = true;
+                hasReq = true;
         }
-        if (hasReq && fromLets)
-            return true;
-    }       
-    
-    return true;
+        if (!hasReq)
+            return false;
+        for (int l = 0; l < word.length(); l++) {
+            boolean fromLets = false;
+            for (int is = 0; is < letters.length; is++) {
+                if (word.charAt(l) == letters[is])
+                    fromLets = true;
+            }
+            if (!fromLets)
+                return false;
+        }
+        return true;
     }
 
     /**
@@ -62,11 +63,11 @@ public class SpellingBee {
     public static void main(String[] args) {
         String[] words = loadFile("words_dropped.txt").split("\n");
         System.out.println("Loaded " + words.length + " words");
-        
+
         // solve me!
         SpellingBee bee = new SpellingBee("ranglty".toCharArray(), 'y');
- 
-        for (int t = 0; t < words.length; t++){
+
+        for (int t = 0; t < words.length; t++) {
             if (bee.checkWord(words[t]) == true)
                 System.out.println(words[t]);
         }
