e386e975537e7271866c5ee8783b53b4ebf76da9
norgy07 <127888520+norgy07@users.noreply.github.com>
2023-04-24 21:45:11 +0000

    Bike

--- unit11/b/Bike.java
+++ unit11/b/Bike.java
@@ -1,6 +1,6 @@
 package unit11.b;
 
-class Vehicle {
+class Vehicle extends Object{
     private int numWheels;
 
     public Vehicle(int numWheels) {
@@ -13,6 +13,23 @@ class Vehicle {
 }
 
 // Code me! bikes have 2 wheels
-public class Bike {
+public class Bike extends Vehicle {
 
+    private String type;
+    private boolean suspension;
+
+    public Bike(){
+        super(2);
+    }
+
+    public Bike(String type, boolean sus){
+        super(2);
+        this.type = type;
+        this.suspension = sus;
+    }
+
+    public static void main (String [] args){
+        Bike bike = new Bike();
+        System.out.println(bike.toString());
+    }
 }
