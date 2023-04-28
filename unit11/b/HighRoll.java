9249da2db46b1ef710831d97cdbd14dcac50f6cb
norgy07 <127888520+norgy07@users.noreply.github.com>
2023-04-28 02:33:18 +0000

    High Roll made the class not much work

---
+++ unit11/b/HighRoll.java
@@ -0,0 +1,15 @@
+package unit11.b;
+
+/*Players roll 3 die and add the total up. The player with the highest total adds the total to their score, 
+ *the others receive no points. If there is a tie for the highest score, then both players earn the points. 
+ *The player to reach 50 first wins.
+ */
+
+public class HighRoll {
+    private int pOne;
+    private int pTwo; 
+
+    public HighRoll(){
+
+    }
+}
